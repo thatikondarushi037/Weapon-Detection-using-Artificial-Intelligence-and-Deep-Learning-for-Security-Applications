e89c4508614b9e1b92e229835629bb4b37f84065
THATIKONDA RUSHI <104714791+thatikondarushi037@users.noreply.github.com>
2024-12-14 19:28:56 +0530

    Add files via upload

---
+++ WeaponDetection/urls.py
@@ -0,0 +1,56 @@
+"""WeaponDetection URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.contrib import admin
+from django.urls import path
+from WeaponDetection import views as mainView
+from users import views as usr
+from admins import views as admins
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", mainView.index, name='index'),
+    path("index/", mainView.index, name="index"),
+    path("logout/", mainView.logout, name="logout"),
+    path("UserLogin/", mainView.UserLogin, name="UserLogin"),
+    path("AdminLogin/", mainView.AdminLogin, name="AdminLogin"),
+    path("UserRegister/", mainView.UserRegister, name="UserRegister"),
+
+    ### User Side Views
+    path("UserRegisterActions/", usr.UserRegisterActions, name="UserRegisterActions"),
+    path("UserLoginCheck/", usr.UserLoginCheck, name="UserLoginCheck"),
+    path("UserHome/", usr.UserHome, name="UserHome"),
+    path("UserSurveillance/", usr.UserSurveillance, name="UserSurveillance"),
+    path("UserViewImages/", usr.UserViewImages, name="UserViewImages"),
+    path("modelTraing/", usr.modelTraing, name="modelTraing"),
+    path("UserImageTest/", usr.UserImageTest, name="UserImageTest"),
+
+
+    ### Admin Side Views
+    path("AdminLoginCheck/", admins.AdminLoginCheck, name="AdminLoginCheck"),
+    path("AdminHome/", admins.AdminHome, name="AdminHome"),
+    path("ViewRegisteredUsers/", admins.ViewRegisteredUsers, name="ViewRegisteredUsers"),
+    path("AdminActivaUsers/", admins.AdminActivaUsers, name="AdminActivaUsers"),
+    path("adminViewModelResults/", admins.adminViewModelResults, name="adminViewModelResults"),
+
+]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
